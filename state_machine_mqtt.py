33c2e818ef6a2878d9cb66e5180767b99b2abda3
noralir <noralr@stud.ntnu.no>
2021-04-25 12:44:23 +0200

    added new manager class

--- state_machine_mqtt.py
+++ state_machine_mqtt.py
@@ -6,11 +6,13 @@ import pyaudio
 import paho.mqtt.client as mqtt
 import paho.mqtt.client as publish
 
-class WalkieTalkie:
 
+
+class WalkieTalkie:
+    '''
     def on_message(self, client, userdata, message):
         return self.driver.send(on_message_receive)
-
+    '''
 
     def __init__(self, name):
         self._logger = logging.getLogger(__name__)
@@ -26,15 +28,16 @@ class WalkieTalkie:
         }
 
         ''' Variables used in multiple functions, defined on init to be reachable '''
-        self.p_out
-        self.stream_out
-        self.frames
+        self.p_out = ''
+        self.stream_out = ''
+        self.frames = []
         self.audiofiles = []
         self.WAVE_OUTPUT_FILENAME = ''
         self.MQTT_BROKER = 'mqtt.item.ntnu.no'
         self.MQTT_TOPIC_INPUT = 'team8/WalkieTalkie/'
         self.MQTT_TOPIC_OUTPUT = 'team8/WalkieTalkie'
         self.MQTT_PORT = 1883
+        '''
         self.mqtt_client = mqtt.Client()
         self.mqtt_client.connect(self.MQTT_BROKER, self.MQTT_PORT)
         self.mqtt_client.subsribe(self.MQTT_TOPIC_INPUT)
@@ -42,12 +45,14 @@ class WalkieTalkie:
         self.mqtt_client.on_message = self.on_message
         self.driver = stmpy.Driver()
         self.driver.start(keep_active=True)
+        '''
 
 
     def create_machine(name):
         ''' Create state machine with helper method '''
         walkie_talkie = WalkieTalkie(name)
         #TODO: Add deferred events? Have to make states and triggers instead
+        
         t0 = {'source': 'initial', 
               'target': 'listening'}
         t1 = {'trigger':'button_press', 
@@ -87,7 +92,7 @@ class WalkieTalkie:
         s_3 = {'name': 'record_message',
             'on_message_receive': 'defer'}
 
-        walkie_talkie_stm = stmpy.Machine(name=name, transitions=[t0, t1, t2, t3, t4, t5, t6, t7], states = [s_1, s_2, s_3])
+        walkie_talkie_stm = stmpy.Machine(name=name, transitions=[t0, t1, t2, t3, t4, t5, t6, t7], states = [s_1, s_2, s_3], obj=walkie_talkie)
         walkie_talkie.stm = walkie_talkie_stm
         return walkie_talkie_stm
 
@@ -201,10 +206,59 @@ class WalkieTalkie:
         self.p_out.terminate()
         self._logger.debug('Recording ignored')
        
-walkie_talkie_stm = WalkieTalkie.create_machine()
-driver.add_machine(walkie_talkie_stm)
+
 
 
 class WalkieTalkieManager():
-    
 
+    def on_connect(self, client, userdata, flags, rc):
+        self._logger.debug('MQTT connected to {}'.format(client))
+    
+    def on_message(self, client, userdata, msg):
+        #self.walkie_talkie_stm.userdata = userdata
+        self.walkie_talkie_stm.send('on_message_receive')
+
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+        print('logging under name {}.'.format(__name__))
+        self._logger.info('Starting Component')
+
+        # create client
+        self.MQTT_BROKER = 'mqtt.item.ntnu.no'
+        self.MQTT_TOPIC_INPUT = 'team8/WalkieTalkie'
+        self.MQTT_TOPIC_OUTPUT = 'team8/WalkieTalkie'
+        self.MQTT_PORT = 1883
+
+        self._logger.debug('Connecting to MQTT broker {} at port {}'.format(self.MQTT_BROKER, self.MQTT_PORT))
+        self.mqtt_client = mqtt.Client()
+        # callback methods
+        self.mqtt_client.on_connect = self.on_connect
+        self.mqtt_client.on_message = self.on_message
+        # Connect to the broker
+        self.mqtt_client.connect(self.MQTT_BROKER, self.MQTT_PORT)
+        # subscribe to proper topic(s) of your choice
+        self.mqtt_client.subscribe(self.MQTT_TOPIC_INPUT)
+        # start the internal loop to process MQTT messages
+        self.mqtt_client.loop_start()
+
+        # we start the stmpy driver, without any state machines for now
+        self.driver = stmpy.Driver()
+        self.driver.start(keep_active=True)
+        self._logger.debug('Component initialization finished')
+
+        #create walkie talkie
+        self.walkie_talkie_stm = WalkieTalkie.create_machine('wt1')
+        self.driver.add_machine(self.walkie_talkie_stm)
+
+
+
+debug_level = logging.DEBUG
+logger = logging.getLogger(__name__)
+logger.setLevel(debug_level)
+ch = logging.StreamHandler()
+ch.setLevel(debug_level)
+formatter = logging.Formatter('%(asctime)s - %(name)-12s - %(levelname)-8s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+w = WalkieTalkieManager()
