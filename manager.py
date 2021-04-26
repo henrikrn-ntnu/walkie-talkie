d2cde52a3e27de1cf3d0cffc78fc9632d27d7b10
noralir <noralr@stud.ntnu.no>
2021-04-26 19:48:53 +0200

    made is work

---
+++ manager.py
@@ -0,0 +1,84 @@
+import logging
+import stmpy
+from datetime import datetime
+import wave
+import pyaudio
+import paho.mqtt.client as mqtt
+import hashlib
+from threading import Thread
+from appJar import gui
+
+#other files:
+import sendreceive as sr
+from statemachine import WalkieTalkie
+from wtgui import WTGUI
+
+class WalkieTalkieManager():    
+    def on_connect(self, client, userdata, flags, rc):
+        self._logger.debug('MQTT connected to {}'.format(client))
+    
+    def on_message(self, client, userdata, msg):
+        if len(msg.payload) == 200:
+            msg_in = msg.payload.decode("utf-8")
+            msg_in = msg_in.split(",,")
+            if msg_in[0] == "header":
+                self.filename = datetime.now().strftime("%H_%M_%S") + ".wav"
+                self.currentfile = open(self.filename, 'wb')
+            elif msg_in[0] == "end":
+                self.currentfile.close()
+                self.walkie_talkie.filename = self.filename
+                self.walkie_talkie_stm.send('on_message_receive')
+        else:
+            self.currentfile.write(msg.payload)
+
+   
+    def __init__(self):
+        self._logger = logging.getLogger(__name__)
+        print('logging under name {}.'.format(__name__))
+        self._logger.info('Starting Component')
+
+        # create client
+        self.MQTT_BROKER = 'mqtt.item.ntnu.no'
+        self.MQTT_TOPIC_INPUT = 'team8/WalkieTalkie'
+        self.MQTT_TOPIC_OUTPUT = 'team8/WalkieTalkie'
+        self.MQTT_PORT = 1883
+
+        self.filename = ''
+        self.currentfile = ''
+
+        self._logger.debug('Connecting to MQTT broker {} at port {}'.format(self.MQTT_BROKER, self.MQTT_PORT))
+        self.mqtt_client = mqtt.Client()
+        # callback methods
+        self.mqtt_client.on_connect = self.on_connect
+        self.mqtt_client.on_message = self.on_message
+        # Connect to the broker
+        self.mqtt_client.connect(self.MQTT_BROKER, self.MQTT_PORT)
+        # subscribe to proper topic(s) of your choice
+        self.mqtt_client.subscribe(self.MQTT_TOPIC_INPUT)
+        # start the internal loop to process MQTT messages
+        self.mqtt_client.loop_start()
+
+        # we start the stmpy driver, without any state machines for now
+        self.driver = stmpy.Driver()
+        self.driver.start(keep_active=True)
+        self._logger.debug('Component initialization finished')
+
+        #create walkie talkie
+        self.walkie_talkie, self.walkie_talkie_stm = WalkieTalkie.create_machine('wt1')
+        self.driver.add_machine(self.walkie_talkie_stm)
+
+        # setup gui
+        gui = WTGUI(self.walkie_talkie_stm)
+
+
+
+debug_level = logging.DEBUG
+logger = logging.getLogger(__name__)
+logger.setLevel(debug_level)
+ch = logging.StreamHandler()
+ch.setLevel(debug_level)
+formatter = logging.Formatter('%(asctime)s - %(name)-12s - %(levelname)-8s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+w = WalkieTalkieManager()
---
+++ statemachine.py
@@ -0,0 +1,201 @@
+import logging
+import stmpy
+from datetime import datetime
+import wave
+import pyaudio
+import paho.mqtt.client as mqtt
+import hashlib
+from threading import Thread
+from appJar import gui
+import time
+
+import sendreceive as sr
+
+class WalkieTalkie:
+
+    def __init__(self, name):
+
+        # logging
+        self._logger = logging.getLogger(__name__)
+        self._logger.setLevel(logging.DEBUG)
+
+        self.name = name
+        self.recording = False
+        ''' Constants '''
+        self.max_recording_time = 60 #seconds
+        self.audioconstants = {
+            'CHUNK': 1024,
+            'FORMAT' : pyaudio.paInt16,
+            'CHANNELS' : 2,
+            'RATE' : 44100
+        }
+
+        ''' Variables used in multiple functions, defined on init to be reachable '''
+        self.filename = ''
+        self.p_out = ''
+        self.stream_out = ''
+        self.frames = []
+        self.audiofiles = []
+        self.WAVE_OUTPUT_FILENAME = ''
+        self.MQTT_BROKER = 'mqtt.item.ntnu.no'
+        self.MQTT_TOPIC_INPUT = 'team8/WalkieTalkie/'
+        self.MQTT_TOPIC_OUTPUT = 'team8/WalkieTalkie'
+        self.MQTT_PORT = 1883
+
+        #connect to broker
+        self.mqtt_client = mqtt.Client()
+        self.mqtt_client.connect(self.MQTT_BROKER, self.MQTT_PORT)
+        self.mqtt_client.loop_start()
+
+    def create_machine(name):
+        ''' Create state machine with helper method '''
+        walkie_talkie = WalkieTalkie(name)
+        
+        t0 = {'source': 'initial', 
+              'target': 'listening'}
+        t1 = {'trigger':'button_press', 
+              'source': 'listening', 
+              'target':'record_message',
+              'effect': 'start_timer("t", 60000)'}
+        t2 = {'trigger': 'button_release', 
+              'source': 'record_message', 
+              'target': 'listening',
+              'effect': 'stop_timer("t"); stop_recording; send_message'}
+        t3 = {'trigger': 'on_message_receive', 
+              'source': 'listening', 
+              'target': 'listening',
+              'effect': 'add_to_list; play_messages'}
+        t4 = {'trigger': 'pause', 
+              'source': 'listening', 
+              'target': 'paused'}
+        t5 = {'trigger': 'on_message_receive', 
+              'source': 'paused', 
+              'target': 'paused',
+              'effect': 'add_to_list'}
+        t6 = {'trigger': 'unpause', 
+              'source': 'paused', 
+              'target': 'listening',
+              'effect': 'play_messages'}
+        t7 = {'trigger': 't',
+              'source': 'record_message',
+              'target': 'listening',
+              'effect': 'ignore_recording'}
+        t8 = {'trigger': 'on_message_receive',
+              'source': 'record_message',
+              'target': 'listening',
+              'a': 'defer'}
+        s_1 = {'name': 'listening', 'button_release' : 'defer'}
+        s_2 = {'name': 'paused'}
+        s_3 = {'name': 'record_message','do': 'start_recording()',
+            'on_message_receive': 'defer'}
+
+        walkie_talkie_stm = stmpy.Machine(name=name, transitions=[t0, t1, t2, t3, t4, t5, t6, t7, t8], states = [s_1, s_2, s_3], obj=walkie_talkie)
+        walkie_talkie.stm = walkie_talkie_stm
+        return walkie_talkie, walkie_talkie_stm
+
+
+    def start_recording(self):
+        #self.stm.start_timer('t', self.max_recording_time * 1000) #Input in milliseconds
+        
+        ''' Init recording '''
+        self.p_out = pyaudio.PyAudio()
+        SPEAKERS = self.p_out.get_default_output_device_info()["hostApi"] 
+        self.stream_out = self.p_out.open(format=self.audioconstants['FORMAT'],
+                        channels=self.audioconstants['CHANNELS'],
+                        rate=self.audioconstants['RATE'],
+                        input=True,
+                        frames_per_buffer = self.audioconstants['CHUNK'],
+                        input_host_api_specific_stream_info=SPEAKERS)
+        
+        ''' Start recording ''' 
+        self._logger.debug('Recording started')
+        self.frames = []
+        self.recording = True
+        while self.recording:
+            data = self.stream_out.read(self.audioconstants['CHUNK'])
+            self.frames.append(data)
+
+    def stop_recording(self):
+        self.recording = False
+        ''' Stop recording '''
+        self.stream_out.stop_stream()
+        self.stream_out.close()
+        self.p_out.terminate()
+        self._logger.debug('Recording stopped')
+
+        ''' Save recording '''
+        self.WAVE_OUTPUT_FILENAME = datetime.now().strftime("%H_%M_%S") + ".wav"
+        wf = wave.open(self.WAVE_OUTPUT_FILENAME, 'wb')
+        wf.setnchannels(self.audioconstants['CHANNELS'])
+        wf.setsampwidth(self.p_out.get_sample_size(self.audioconstants['FORMAT']))
+        wf.setframerate(self.audioconstants['RATE'])
+        wf.writeframes(b''.join(self.frames))
+        wf.close() 
+        self._logger.debug('Recording written to file: {}'.format(self.WAVE_OUTPUT_FILENAME))
+
+    def send_message(self):
+        filename = self.WAVE_OUTPUT_FILENAME
+        self._logger.debug('Start sending message')
+        buffer_list = sr.send_audiofile(filename)
+        self._logger.debug('Received buffer_list, start sending packets {}'.format(buffer_list))
+        
+        for packet in buffer_list: #TODO: why this only run once i am lost
+            self._logger.debug('Sending packet')
+            #sr.c_publish(self.mqtt_client, self.MQTT_TOPIC_OUTPUT, packet, qos=1)
+            #self.mqtt_client.publish(self.MQTT_TOPIC_OUTPUT, packet)
+            self.mqtt_client.publish(self.MQTT_TOPIC_OUTPUT, packet, qos=2)  # publish
+            time.sleep(1)
+            #if res == 0:  # published ok
+            '''
+            if sr.wait_for(self.MQTT_TOPIC_OUTPUT, "PUBACK", running_loop=True):
+                self.mqtt_client.puback_flag = False  # reset flag
+            else:
+                raise SystemExit("not got puback so quitting")
+            '''
+
+        self._logger.debug('Packets sent')
+        self.WAVE_OUTPUT_FILENAME = '' # remove filename just to be safe 
+
+    def add_to_list(self):
+        self.audiofiles.append(self.filename)
+        self._logger.debug('Received recording added to list')
+        self.filename = ''
+
+    def play_messages(self):
+        self._logger.debug('Initialize playing messages')
+        while len(self.audiofiles) > 0:
+            chunk = self.audioconstants['CHUNK']
+
+            """ Init audio stream """ 
+            try: #This try may be useless because of the while loop
+                file = self.audiofiles[0]
+                self.audiofiles.pop(0)
+                wf = wave.open(file, 'rb')
+                p_in = pyaudio.PyAudio()
+                stream_in = p_in.open(
+                    format = p_in.get_format_from_width(wf.getsampwidth()),
+                    channels = wf.getnchannels(),
+                    rate = wf.getframerate(),
+                    output = True)
+                self._logger.debug('Recording ready to play')
+            except Exception as e:
+                self._logger.error('Something wrong with file. {}'.format(e))
+                return
+
+            """ Play entire file """
+            data = wf.readframes(chunk)
+            while len(data) > 0:
+                stream_in.write(data)
+                data = wf.readframes(chunk)
+            self._logger.debug('Recording played')
+
+            """ Graceful shutdown """ 
+            stream_in.close()
+            p_in.terminate()
+
+    def ignore_recording(self):
+        ''' Stop recording '''
+        self.stream_out.stop_stream()
+        self.stream_out.close()
+        self.p_out.terminate()
+        self._logger.debug('Recording ignored')
